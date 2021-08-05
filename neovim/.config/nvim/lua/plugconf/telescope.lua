92a069d670c19b6df5bba4dfc1ba48248f5659dc
Marcos <arkanus@gmail.com>
2021-08-05 16:39:57 -0400

    nvim: telescope binding to send matches to qflist

--- neovim/.config/nvim/lua/plugconf/telescope.lua
+++ neovim/.config/nvim/lua/plugconf/telescope.lua
@@ -1,3 +1,15 @@
+local actions = require "telescope.actions"
+
+require'telescope'.setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["Q"] = actions.send_to_qflist,
+      }
+    }
+  }
+}
+
 local function setKeyMaps(maps)
     for _, map in ipairs(maps) do
         vim.cmd(map)
