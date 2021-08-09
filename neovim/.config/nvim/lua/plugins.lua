79030d84ae16964f7334e37d2c90ac54931d030a
Marcos <arkanus@gmail.com>
2021-08-09 10:10:22 -0400

    nvim: remove unused block, config startify

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -8,24 +8,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
     execute 'packadd packer.nvim'
 end
 
---- Check if a file or directory exists in this path
-local function require_plugin(plugin)
-    local plugin_prefix = fn.stdpath('data') .. '/site/pack/packer/opt/'
-
-    local plugin_path = plugin_prefix .. plugin .. '/'
-    --	print('test '..plugin_path)
-    local ok, err, code = os.rename(plugin_path, plugin_path)
-    if not ok then
-        if code == 13 then
-            -- Permission denied, but it exists
-            return true
-        end
-    end
-    --	print(ok, err, code)
-    if ok then vim.cmd('packadd ' .. plugin) end
-    return ok, err, code
-end
-
 -- Auto compile when there are changes in plugins.lua
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
@@ -71,8 +53,8 @@ return require('packer').startup(function(use)
     require('plugconf.kommentary')
 
     use 'mhinz/vim-startify'
+    require 'plugconf.startify'
 
     -- plugin configs
     require('plugconf.telescope')
 end)
-
