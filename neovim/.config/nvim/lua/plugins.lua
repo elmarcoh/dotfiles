beb14d2a3aba671cc1db50d793dedae2fdf9e7bf
Marcos <arkanus@gmail.com>
2021-08-19 17:01:04 -0400

    nvim: 🔌 circular icons

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -38,7 +38,10 @@ return require('packer').startup(function(use)
 
     use {
       'kyazdani42/nvim-tree.lua',
-      requires = 'kyazdani42/nvim-web-devicons',
+      requires ={ 'kyazdani42/nvim-web-devicons', 'projekt0n/circles.nvim'},
+      config = function()
+        require'circles'.setup()
+      end
     }
 
     -- This makes diagnostics to lose color and underlines :'(
