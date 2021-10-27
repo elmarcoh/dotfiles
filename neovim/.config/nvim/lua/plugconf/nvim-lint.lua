5c85333f6d60a68c7dd0a7fd4365be79fe04a06b
elmarcoh <arkanus@gmail.com>
2021-10-27 17:13:58 -0300

    nvim: run lint for files on save

--- neovim/.config/nvim/lua/plugconf/nvim-lint.lua
+++ neovim/.config/nvim/lua/plugconf/nvim-lint.lua
@@ -2,4 +2,4 @@ require('lint').linters_by_ft = {
   python = {'flake8',},
 }
 
-vim.api.nvim_command("au BufWritePost <buffer> lua require('lint').try_lint()")
+vim.cmd "autocmd BufWritePost *.py lua require('lint').try_lint()"
