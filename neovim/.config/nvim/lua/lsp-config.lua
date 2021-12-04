6c58de7a88684951406ac3e3429a22769c302598
Marcos <arkanus@gmail.com>
2021-12-03 23:16:56 -0300

    nvim: fix LSP gutter icons, comment unused keybind

--- neovim/.config/nvim/lua/lsp-config.lua
+++ neovim/.config/nvim/lua/lsp-config.lua
@@ -69,6 +69,6 @@ vim.lsp.protocol.CompletionItemKind = {
 -- gutter (side column) icons
 local signs = {Error=" ", Warning=" ", Hint=" ", Information=" "}
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
+  local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, {text=icon, texthl=hl, numhl=""})
 end
