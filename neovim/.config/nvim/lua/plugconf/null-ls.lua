5312c94f09090589905004a5de0552c4ff1f2025
elmarcoh <arkanus@gmail.com>
2022-01-27 19:55:41 -0300

    nvim: add pycln to null-ls config

--- neovim/.config/nvim/lua/plugconf/null-ls.lua
+++ neovim/.config/nvim/lua/plugconf/null-ls.lua
@@ -1,5 +1,24 @@
 local builtins = require("null-ls").builtins
 
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+local pycln = h.make_builtin({
+    name = "pycln",
+    method = FORMATTING,
+    filetypes = { "python" },
+    generator_opts = {
+        command = "pycln",
+        args = {
+            "$FILENAME",
+        },
+        to_temp_file = true,
+    },
+    factory = h.formatter_factory,
+})
+
 require("null-ls").setup({
 	sources = {
 
@@ -9,6 +28,7 @@ require("null-ls").setup({
 
 		-- python
 		builtins.diagnostics.flake8,
+		pycln,
 		builtins.formatting.black,
 		builtins.formatting.isort,
 	},
