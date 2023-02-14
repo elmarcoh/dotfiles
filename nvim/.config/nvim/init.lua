1a54cc196001e0a32c78ddd8649069cab79f5973
Marcos <arkanus@gmail.com>
2023-02-14 02:15:09 -0300

    nvim: keymap for file formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -305,6 +305,7 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>ff', vim.lsp.buf.format, '[F]ormat [F]ile')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
