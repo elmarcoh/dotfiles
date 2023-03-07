510cca6404dd193cc6c30cef40de30434e8a7df2
Marcos <arkanus@gmail.com>
2023-03-06 22:46:35 -0300

    nvim: add startuup screen plugin (alpha)

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -76,5 +76,13 @@ return {
       return vim.fn.executable("make") == 1
     end,
   },
+
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+  },
 }
 -- vim: ts=2 sts=2 sw=2 expandtab
