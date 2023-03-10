fcca89c06baf217fbee32d7ef27f6ec4dd1b99a1
Marcos <arkanus@gmail.com>
2023-03-10 01:39:27 -0300

    nvim: configure null-ls formatting

--- nvim/.config/nvim/lua/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/plugins/null_ls.lua
@@ -2,8 +2,12 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
     null_ls.setup({
-      sources = { null_ls.builtins.formatting.stylua },
+      sources = {
+        formatting.stylua,
+        formatting.prettier,
+      },
     })
   end,
 }
