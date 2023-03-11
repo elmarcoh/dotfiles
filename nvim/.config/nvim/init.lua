bb13b7957311c5740a8ec9d521274ecab49900e1
Marcos <arkanus@gmail.com>
2023-03-11 00:09:35 -0300

    nvim: add keymap for formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -112,7 +112,10 @@ local on_attach = function(_, bufnr)
 	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
 		vim.lsp.buf.format()
 	end, { desc = "Format current buffer with LSP" })
+
+   nmap("<leader>fm", vim.lsp.buf.format, "[F]or[M]at current file")
 end
+
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
