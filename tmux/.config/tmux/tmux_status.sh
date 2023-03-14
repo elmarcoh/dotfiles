43501c562ae995865d7ce9e3b86b43360ed4ee97
Marcos <arkanus@gmail.com>
2023-03-13 23:19:06 -0300

    tmux: add swap and vpn to statusbsr

--- tmux/.config/tmux/tmux_status.sh
+++ tmux/.config/tmux/tmux_status.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function memory_usage() {
-  read used total <<< $(free -m | awk '/Mem/{printf $2" "$3}')
+  read used total <<< $(free -m | awk "/$1/{printf \$2\" \"\$3}")
   percent=$((100 * $total / $used))
-  printf ' %.1f%%' $percent
+  printf "%s %.1f%%" $2 $percent
 }
 
 function vpn_connection() {
-  [ -d /sys/class/net/tun0 ] && printf "VPN"
+  [ -d /sys/class/net/tun0 ] && printf "[]"
 }
 
 function container_count() {
@@ -15,8 +15,10 @@ function container_count() {
 }
 
 function main() {
-  printf "%s | %s" \
+  printf "%s %s | %s | %s" \
+    "$(vpn_connection)" \
     "$(container_count)" \
-    "$(memory_usage)"
+    "$(memory_usage Mem )" \
+    "$(memory_usage Swap 󰾴)"
 }
 main
