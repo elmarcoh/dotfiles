3a50f9e661f26cf0d4b0e3f8a7af3399a01a24cd
Marcos <arkanus@gmail.com>
2023-06-19 17:16:16 -0400

    nvim: solve startup errors

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -11,7 +11,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
 
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { "j-hui/fidget.nvim", opts = {} },
+      { "j-hui/fidget.nvim", opts = {}, tag='legacy' },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       "folke/neodev.nvim",
