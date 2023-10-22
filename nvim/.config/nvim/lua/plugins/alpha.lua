0552df7b145cb3df33258d8797e854d0802003a1
Marcos <arkanus@gmail.com>
2023-10-22 18:31:35 -0300

    nvim: alpha.nvim config

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -2,7 +2,9 @@ return {
   "goolord/alpha-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("alpha").setup(require("alpha.themes.startify").config)
+    local startify = require("alpha.themes.startify")
+    -- startify.nvim_web_devicons.enabled = false
+    require("alpha").setup(startify.config)
     vim.keymap.set("n", "<leader>al", ":Alpha<cr>", { desc = "Open Alpha start page" })
   end,
 }
