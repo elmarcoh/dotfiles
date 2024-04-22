d1f478dd7fe701b2280ab801e650518f5f010d99
Marcos <arkanus@gmail.com>
2024-04-21 20:55:39 -0400

    nvim: put gitsigns config in init and load on open file

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -18,7 +18,26 @@ return {
     "folke/which-key.nvim",
     opts = {},
   },
+  {
+    -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      current_line_blame = true,
+    },
 
+    keys = {
+      { "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>",              desc = "[G]itsigns [H]unk" },
+      { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "[G]itsigns [B]lame toggle" },
+    },
+  },
   -- colorschemes
   -- {
   --   "folke/tokyonight.nvim",
