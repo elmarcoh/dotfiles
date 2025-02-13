524674c0c7d1fb0211b87f431f7a710944b5fdb6
Marcos <arkanus@gmail.com>
2025-02-13 01:00:55 -0300

    nvim: tidy up keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -15,19 +15,26 @@ vim.keymap.set("n", ";d", ":bd<cr>", { silent = true })
 vim.keymap.set("n", ";w", ":w<cr>", { silent = true })
 
 -- Remap for clipboard copy/paste
-vim.keymap.set("v", "<leader>y", '"+y', { desc = "Clipboard Yank" })
-vim.keymap.set("n", "<leader>y", '"+y', { desc = "Clipboard Yank" })
-vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Clipboard Yank" })
-vim.keymap.set("v", "<leader>p", '"+p', { desc = "Clipboard Paste" })
-vim.keymap.set("n", "<leader>p", '"+p', { desc = "Clipboard Paste" })
-vim.keymap.set("n", "<leader>P", '"+P', { desc = "Clipboard Paste" })
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Clipboard Yank" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Clipboard Paste" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Clipboard Line Yank" })
+vim.keymap.set("n", "<leader>P", '"+P', { desc = "Clipboard Line Paste" })
 
 -- Buffer navigation
 vim.keymap.set("n", "<Tab>", ":bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<cr>", { desc = "Previous buffer" })
 
+-- Pane navigation
+vim.keymap.set("n", "s", "<Nop>", { desc = "Splits" }) -- disable default binding for `s`
+vim.keymap.set("n", "ss", "<C-w><C-w>", { desc = "Cycle splits" })
+vim.keymap.set("n", "sh", "<C-w>h", { desc = "Left split" })
+vim.keymap.set("n", "sj", "<C-w>j", { desc = "Down split" })
+vim.keymap.set("n", "sk", "<C-w>k", { desc = "Up split" })
+vim.keymap.set("n", "sl", "<C-w>l", { desc = "Right split" })
+
+-- Fast write
 vim.keymap.set("n", "<leader>w", ":w<cr>", { desc = "[W]rite file" })
---
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
