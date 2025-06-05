c755fdc774c271f630aaaa0c6bd83cb06821b241
Marcos <arkanus@gmail.com>
2025-06-05 00:29:20 -0400

    tmux: remove `bc` dependency

--- tmux/.config/tmux/tmux_status.sh
+++ tmux/.config/tmux/tmux_status.sh
@@ -2,7 +2,7 @@
 
 function memory_usage() {
   read used total <<< $(free -m | awk "/$1/{printf \$2\" \"\$3}")
-  percent=$(bc <<< "scale=1; 100 * $total / $used")
+  percent="$(( 100 * $total / $used ))"
   echo $2 $percent"%"
 }
 
