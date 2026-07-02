e39b9240fb12e0fe7970b0679f53c5a0c6a3ec3b
Marcos <arkanus@gmail.com>
2026-07-01 22:45:01 -0400

    nvim: remove luasnips, update some plugins
    - not using luasnips since LSP often provide snippets

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -72,13 +72,13 @@ return {
 		},
 	},
 
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
+	-- {
+	-- 	"L3MON4D3/LuaSnip",
+	-- 	dependencies = { "rafamadriz/friendly-snippets" },
+	-- 	config = function()
+	-- 		require("luasnip.loaders.from_vscode").lazy_load()
+	-- 	end,
+	-- },
 
 	-- dreaded copilot 💀
 	-- { "github/copilot.vim" },
